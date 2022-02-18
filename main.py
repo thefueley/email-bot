a185c52532b4c31fc6d93b54d123e39ab1455090
TheFueley <cmrive2@gmail.com>
2022-02-18 11:28:59 -0500

    black and isort

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
+import os
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-import os
 
-smpt_host = os.environ['SMTP_HOST']
-sender = os.environ['SMTP_ACCOUNT']
-sender_pass = os.environ['SMTP_PASSWORD']
-recipient = os.environ['SMTP_RECEIVER']
+smpt_host = os.environ["SMTP_HOST"]
+sender = os.environ["SMTP_ACCOUNT"]
+sender_pass = os.environ["SMTP_PASSWORD"]
+recipient = os.environ["SMTP_RECEIVER"]
 body = "A test email from Python"
 subject = "ΦΟΒΟΣ"
 
-msg = MIMEMultipart('alternative')
+msg = MIMEMultipart("alternative")
 
-msg['From'] = sender
-msg['To'] = recipient
-msg['subject'] = subject
+msg["From"] = sender
+msg["To"] = recipient
+msg["subject"] = subject
 
 text = "Hi!\nHow are you?\nHere is the link you wanted:\nhttp://www.python.org"
 html = """\
@@ -29,15 +29,15 @@ html = """\
 </html>
 """
 
-part1 = MIMEText(text, 'plain')
-part2 = MIMEText(html, 'html')
+part1 = MIMEText(text, "plain")
+part2 = MIMEText(html, "html")
 
 msg.attach(part1)
 msg.attach(part2)
 
 try:
     # I have non-ascii in my hostname. `local_hostname` required for me.
-    mailserver = smtplib.SMTP(smpt_host, 587, local_hostname='woof@local')
+    mailserver = smtplib.SMTP(smpt_host, 587, local_hostname="woof@local")
     # mailserver.set_debuglevel(True)
     mailserver.starttls()
     mailserver.login(sender, sender_pass)
